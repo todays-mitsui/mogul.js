e112745e6535b89d85a86664d011a371d4ef99d2
Mitsui <todays.mitsui@gmail.com>
2018-05-18 06:25:25 +0900

    .

---
+++ src/js/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div role="mogul-app">
+    <mogul-input></mogul-input>
+    <mogul-result></mogul-result>
+  </div>
+</template>
+
+<script>
+import MogulInput  from './Components/MogulInput.vue';
+import MogulResult from './Components/MogulResult.vue';
+
+module.exports = {
+  components: {
+    MogulInput,
+    MogulResult,
+  },
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/js/Components/MogulInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <input
+    type="text"
+    v-model="expr"
+  >
+</template>
+
+<script>
+import Expr from '../Types/Expr';
+
+module.exports = {
+  data: function() {
+    return {
+      expr: 'foobar',
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
