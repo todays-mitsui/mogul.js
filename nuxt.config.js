27ae56f4bb8a54886bf9e35570fb466a7887b86e
Mitsui <todays.mitsui@gmail.com>
2019-04-14 23:20:59 +0900

    chore: title 設定

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,7 +9,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: pkg.name,
+    title: 'Mogul ⛷ | evaluates λ-calculations step by step.',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
