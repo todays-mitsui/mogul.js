2d836f1ed3477d539e9d2dc7a6400e3b501692a2
Mitsui <todays.mitsui@gmail.com>
2019-05-04 17:20:56 +0900

    feat: 例を拡充

--- app/components/StdIn.vue
+++ app/components/StdIn.vue
@@ -25,8 +25,13 @@ export default {
   data() {
     const defaultInputs = [
       's(k, k)(:x)',
+      '6(:f, :x)',
       'and(true, or(false, true))',
-      'eq(add(2, 1), 3)'
+      'isZero(pred(pred(pred(3))))',
+      'eq(add(2, 1), 3)',
+      'myNumList = cons(1, cons(2, cons(3, Nil)))',
+      'flip(f) = x => y => f(y, x)',
+      '? add'
     ]
 
     return {
